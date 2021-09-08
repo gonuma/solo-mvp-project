cba92255f940604f406754b1a2f3849f430fb8c7
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-08 14:30:29 +0900

    Changed comment to not reference songs

--- migrations/20210906034522_init.js
+++ migrations/20210906034522_init.js
@@ -18,12 +18,7 @@ exports.up = async function (knex) {
   await knex.schema.createTable("comments", (table) => {
     table.increments("id", { primaryKey: true });
 
-    table.string("song_name", { deferrable: "deferred" });
-
-    table
-      .foreign("song_name")
-      .references("songs.song_name")
-      .onDelete("CASCADE");
+    table.string("song_name");
 
     table.string("comment");
   });
