23f1ed2804b34b7b24687de960e0abc33d900f96
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-08 12:55:37 +0900

    Slight change to FK restraint

--- migrations/20210906034522_init.js
+++ migrations/20210906034522_init.js
@@ -19,10 +19,9 @@ exports.up = function (knex) {
     knex.schema.createTable("comments", (table) => {
       table.increments("id", { primaryKey: true });
 
-      table
-        .string("song_name")
-        .references("songs.song_name")
-        .onDelete("CASCADE");
+      table.integer("song_id").references("songs.id").onDelete("CASCADE");
+
+      table.string("song_name");
 
       table.string("comment");
     }),
