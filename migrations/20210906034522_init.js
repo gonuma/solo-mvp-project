a92b9bc8d11bad985ba945e5165068214330a12b
gonuma <81454703+gonuma@users.noreply.github.com>
2021-09-08 14:10:00 +0900

    Changed seeding and schema slightly

--- migrations/20210906034522_init.js
+++ migrations/20210906034522_init.js
@@ -1,32 +1,30 @@
-exports.up = function (knex) {
-  return Promise.all([
-    knex.schema.createTable("users", (table) => {
-      table.increments("id", { primaryKey: true }).notNullable().unique();
+exports.up = async function (knex) {
+  await knex.schema.createTable("users", (table) => {
+    table.increments("id", { primaryKey: true }).notNullable().unique();
 
-      table.string("username").notNullable();
-    }),
+    table.string("username").notNullable();
+  });
 
-    knex.schema.createTable("songs", (table) => {
-      table.increments("id", { primaryKey: true }).notNullable().unique();
+  await knex.schema.createTable("songs", (table) => {
+    table.increments("id", { primaryKey: true }).notNullable().unique();
 
-      table.string("band_name").notNullable();
+    table.string("band_name").notNullable();
 
-      table.string("song_name").notNullable().unique();
+    table.string("song_name").notNullable().unique();
 
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-    }),
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 
-    knex.schema.createTable("comments", (table) => {
-      table.increments("id", { primaryKey: true });
+  await knex.schema.createTable("comments", (table) => {
+    table.increments("id", { primaryKey: true });
 
-      table
-        .string("song_name")
-        .references("songs.song_name")
-        .onDelete("CASCADE");
+    table
+      .string("song_name", { deferrable: "deferred" })
+      .references("songs.song_name")
+      .onDelete("CASCADE");
 
-      table.string("comment");
-    }),
-  ]);
+    table.string("comment");
+  });
 };
 
 exports.down = function (knex) {};
